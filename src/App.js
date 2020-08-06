556ada652501eee819b9d2943bd729bc52e629dd
MugishaU <muwiragiye123@gmail.com>
2020-08-06 12:55:51 +0100

    add navigavtion

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Welcome from "./Container/Welcome";
 import CounterContainer from "./Container/CounterContainer";
 import ThingsContainer from "./Container/ThingsContainer";
 import Error404 from "./Container/Error404";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, NavLink } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <NavLink to="/" activeClassName="current">
+          Home{"   "}
+        </NavLink>
+        <NavLink to="/counter" activeClassName="current">
+          Counter{"   "}
+        </NavLink>
+        <NavLink to="/things" activeClassName="current">
+          Things{"    "}
+        </NavLink>
         <button>Back</button>
+
         <Switch>
           <Route exact path="/" component={Welcome} />
           <Route path="/counter" component={CounterContainer} />
