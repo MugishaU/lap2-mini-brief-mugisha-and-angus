cf7dd1fbf6e4c1b879a85cfa77a7e67048c70b69
MugishaU <muwiragiye123@gmail.com>
2020-08-10 18:05:54 +0100

    remove test h1 from code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>hi</h1>
         <NavLink to="/" activeClassName="current">
           Home{"   "}
         </NavLink>
