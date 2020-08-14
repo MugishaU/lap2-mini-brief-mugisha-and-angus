6801269f395efb148a01ab71457a66aeb71a1f71
MugishaU <muwiragiye123@gmail.com>
2020-08-14 10:12:36 +0100

    remove counter from app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Welcome from "./Container/Welcome";
-import CounterContainer from "./Container/CounterContainer";
 import ThingsContainer from "./Container/ThingsContainer";
 import Error404 from "./Container/Error404";
 import { Switch, Route, NavLink, withRouter } from "react-router-dom";
@@ -13,9 +12,6 @@ class App extends React.Component {
           <NavLink to="/" activeClassName="current">
             Home{"   "}
           </NavLink>
-          {/* <NavLink to="/counter" activeClassName="current">
-          Counter{"   "}
-        </NavLink> */}
           <NavLink to="/things" activeClassName="current">
             List{"    "}
           </NavLink>
